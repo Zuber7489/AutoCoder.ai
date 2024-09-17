17067aad0dd25207a67c63eb6359747a71fcfc69
Zuber7489 <zs808081@gmail.com>
2024-09-17 12:08:22 +0530

    'update'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,6 @@ import { Clipboard } from '@angular/cdk/clipboard';  // Import Clipboard from An
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private texts: string[] = [
-    'Ask Me Anything...',
-    'Generate Code',
-    'Generate Website',
-    'Generate HTML CSS'
-  ];
-  private index: number = 0;
-  private interval: any;
   title = 'code_gen';
   generatedContent: string = '';
   prompt: string = '';
@@ -28,40 +20,15 @@ export class AppComponent {
   askmeanything:boolean=true;
   constructor(private geminiApi: GeminiApiService,private sanitizer: DomSanitizer,private clipboard: Clipboard) {}
   ngOnInit(): void {
-    this.startTextTransition();
   }
 
-  startTextTransition(): void {
-    const transitionText = document.getElementById('transitionText');
-    if (!transitionText) return;
 
-    this.typeText(transitionText, this.texts[this.index]);
-  }
-
-  typeText(element: HTMLElement, text: string) {
-    let i = 0;
-    element.textContent = '';
-    
-    const typingInterval = setInterval(() => {
-      element.textContent += text.charAt(i);
-      i++;
-      if (i > text.length) {
-        clearInterval(typingInterval);
-        setTimeout(() => {
-          this.index = (this.index + 1) % this.texts.length;
-          this.typeText(element, this.texts[this.index]);
-        }, 2000); // Pause before next text starts typing
-      }
-    }, 100); // Adjust typing speed
-  }
   async generateCode() {
     this.askmeanything=false;
     this.isLoading = true;
     try {
-
       // Call the generateContent method with the user prompt
       let response = await this.geminiApi.generateCode(this.prompt);
-
          // Remove backticks using regex
          response = response.replace(/```/g, '');
          response = response.replace(/^html\s+/i, '');
@@ -81,7 +48,6 @@ export class AppComponent {
     const blob = new Blob([code], { type: 'text/html' });
     const url = URL.createObjectURL(blob);
     this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-
     // Clean up old blob URLs to prevent memory leaks
     setTimeout(() => URL.revokeObjectURL(url), 10000);
   }
@@ -90,7 +56,6 @@ export class AppComponent {
     const codeBlock = document.getElementById('codeBlock')?.textContent || '';
     this.clipboard.copy(codeBlock);
     this.copied = true;
-
     // Hide the "Copied" message after 2 seconds
     setTimeout(() => {
       this.copied = false;
